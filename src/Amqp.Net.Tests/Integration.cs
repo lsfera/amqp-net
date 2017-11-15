1ac6267db39c22f9fb3a3910db75cf03dfd88f05
giordanol <luca.giordano@ynap.com>
2017-11-15 16:18:09 +0100

    Added RabbitMqFixture as one time setup/teardown test class

--- src/Amqp.Net.Tests/Integration.cs
+++ src/Amqp.Net.Tests/Integration.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Net;
-using System.Threading;
 using System.Threading.Tasks;
 using Amqp.Net.Client;
 using Amqp.Net.Client.Entities;
 using DotNetty.Common.Internal.Logging;
-using Newtonsoft.Json.Linq;
 using RabbitMqHttpApiClient.API;
 using Xunit;
 using Xunit.Abstractions;
@@ -15,119 +11,72 @@ using Connection = Amqp.Net.Client.Connection;
 
 namespace Amqp.Net.Tests
 {
-    public class Integration : IAsyncLifetime, IDisposable
+    public class Integration : IClassFixture<RabbitMqFixture>
     {
-        private const string DockerNetworkName = "bridgeWhaleNet";
-        private const string RabbitImageName = "rabbitmq";
-        private const string RabbitImageTag = "management";
-        private const string RabbitContainerName = "rmq";
-        private const int DefaultTimeoutSeconds = 10;
-
-        private readonly ITestOutputHelper outputHelper;
-        private readonly DockerProxy dockerProxy;
+        private readonly RabbitMqApi rabbitMqManagementApi;
 
         public Integration(ITestOutputHelper testOutputHelper)
         {
-            outputHelper = testOutputHelper;
-            dockerProxy = new DockerProxy(new Uri(Configuration.DockerHttpApiUri));
-        }
-
-        public async Task InitializeAsync()
-        {
-            await DisposeAsync();
-
-            await dockerProxy.CreateNetworkAsync(DockerNetworkName);
-
-            await dockerProxy.PullImageAsync(RabbitImageName, RabbitImageTag);
-            var portMappings = new Dictionary<string, ISet<string>>
-            {
-                { "4369", new HashSet<string>(){ "4369" } },
-                { "5671", new HashSet<string>(){ "5671" } },
-                { "5672", new HashSet<string>(){ "5672" } } ,
-                { "15671",new HashSet<string>(){ "15671" } },
-                { "15672",new HashSet<string>(){ "15672" } },
-                { "25672",new HashSet<string>(){ "25672" } }
-            };
-            var envVars = new List<string> { "RABBITMQ_DEFAULT_VHOST=test" };
-            var containerId = await dockerProxy.CreateContainerAsync($"{RabbitImageName}:{RabbitImageTag}", RabbitContainerName, portMappings, DockerNetworkName, envVars);
-            await dockerProxy.StartContainerAsync(containerId);
-            await WaitForRabbitMqReady(new CancellationTokenSource(TimeSpan.FromSeconds(DefaultTimeoutSeconds)).Token);
+            var rabbitMqUrl = $"http://{Configuration.RabbitMqHost}:{Configuration.RabbitMqManagementPort}";
+            rabbitMqManagementApi = rabbitMqManagementApi = new RabbitMqApi(rabbitMqUrl, Configuration.RabbitMqUser, Configuration.RabbitMqPassword);
+            //InternalLoggerFactory.DefaultFactory.AddProvider(new TestOtputLoggerProvider(testOutputHelper));
         }
 
         [Fact]
         public async Task CreateExchange()
         {
-            InternalLoggerFactory.DefaultFactory.AddProvider(new TestOtputLoggerProvider(outputHelper));
+            const string exchangeName = "test-xchg";
+            const ExchangeType exchangeType = ExchangeType.Direct;
+            const bool isDurable = true;
+            const bool isAutodelete = false;
+            const bool isInternal = false;
 
-            const string testXchg = "test-xchg";
-            var ipEndPoint = new IPEndPoint(IPAddress.Parse(Configuration.RabbitMqHost), Configuration.RabbitMqClientPort);
-            var networkCredential = new NetworkCredential(Configuration.RabbitMqUser, Configuration.RabbitMqPassword);
-            var connectionString = new ConnectionString(ipEndPoint, networkCredential, Configuration.RabbitMqVirtualHost);
-            using (var connection = await Connection.ConnectAsync(connectionString))
-            using (var channel = await connection.OpenChannelAsync())
-            {
-                await channel.ExchangeDeclareAsync(testXchg, ExchangeType.Direct, true, false, false);
-            }
-
-            var retrievedExchangeName = await new RabbitMqApi($"http://{Configuration.RabbitMqHost}:{Configuration.RabbitMqManagementPort}",
-                Configuration.RabbitMqUser, Configuration.RabbitMqPassword).GetExchangeByVhostAndName(Configuration.RabbitMqVirtualHost, testXchg).ConfigureAwait(false);
-            Assert.NotNull(retrievedExchangeName);
-        }
-
-        public async Task DisposeAsync()
-        {
-            await dockerProxy.StopContainerAsync(RabbitContainerName);
-            await dockerProxy.RemoveContainerAsync(RabbitContainerName);
-            await dockerProxy.DeleteNetworksAsync(DockerNetworkName);
-        }
-
-        public void Dispose()
-        {
-            dockerProxy.Dispose();
-        }
-
-        private async Task WaitForRabbitMqReady(CancellationToken token)
-        {
-            while (true)
-            {
-                token.ThrowIfCancellationRequested();
-                if (IsRabbitMqReady())
-                    return;
-                await Task.Delay(500, token);
-            }
-        }
-
-        private bool IsRabbitMqReady()
-        {
-            var path = $"aliveness-test/{Configuration.RabbitMqVirtualHost.Replace("/", "%2f")}";
-            var requestUri = new Uri($"http://{Configuration.RabbitMqHost}:{Configuration.RabbitMqManagementPort}/api/{path}");
-            var webRequest = (HttpWebRequest)WebRequest.Create(requestUri);
-            webRequest.Credentials = (ICredentials)new NetworkCredential(Configuration.RabbitMqUser, Configuration.RabbitMqPassword);
-
+            IConnection connection = null;
+            IChannel channel = null;
             try
             {
-                var response = MakeRequest(webRequest);
-                return JObject.Parse(response).SelectToken("status").Value<string>() == "ok";
+                (connection, channel) = await BootstrapRabbit();
+                await channel.ExchangeDeclareAsync(exchangeName, exchangeType, isDurable, isAutodelete, isInternal);
             }
-            catch
+            finally
             {
-                return false;
+                channel?.Dispose();
+                connection?.Dispose();
             }
+
+            var retrievedExchange = await rabbitMqManagementApi.GetExchangeByVhostAndName(Configuration.RabbitMqVirtualHost, exchangeName);
+
+            Assert.NotNull(retrievedExchange);
+            Assert.Equal(retrievedExchange.Name, exchangeName);
+            Assert.Equal(retrievedExchange.Type, exchangeType.ToString(), StringComparer.OrdinalIgnoreCase);
+            Assert.Equal(retrievedExchange.Durable, isDurable);
+            Assert.Equal(retrievedExchange.AutoDelete, isAutodelete);
+            Assert.Equal(retrievedExchange.Internal, isInternal);
         }
 
-        private static string MakeRequest(WebRequest webRequest)
+        private Task<(IConnection connection, IChannel channel)> BootstrapRabbit()
         {
-            using (var httpResponse = (HttpWebResponse) webRequest.GetResponse())
-            {
-                if (httpResponse.StatusCode != HttpStatusCode.OK)
-                    throw new Exception($"Status code {httpResponse.StatusCode}");
+            IConnection connection = null;
+            IChannel channel = null;
+            var ipAddress = IPAddress.Parse(Configuration.RabbitMqHost);
+            var ipEndPoint = new IPEndPoint(ipAddress, Configuration.RabbitMqClientPort);
+            var networkCredential = new NetworkCredential(Configuration.RabbitMqUser, Configuration.RabbitMqPassword);
+            var connectionString = new ConnectionString(ipEndPoint, networkCredential, Configuration.RabbitMqVirtualHost);
 
-                using (var responseStream = httpResponse.GetResponseStream())
-                using (var streamReader = new StreamReader(responseStream))
-                {
-                    return streamReader.ReadToEnd();
-                }
+            Task<IChannel> OpenConnection(Task<IConnection> _)
+            {
+                connection = _.Result;
+                return connection.OpenChannelAsync();
             }
+
+            return Connection.ConnectAsync(connectionString)
+                .ContinueWith(OpenConnection).Unwrap()
+                .ContinueWith(__ =>
+                {
+                    channel = __.Result;
+                    return (connection, channel);
+                });
+
         }
     }
 }
---
+++ src/Amqp.Net.Tests/RabbitMqFixture.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DotNetty.Common.Internal.Logging;
+using RabbitMqHttpApiClient.API;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Amqp.Net.Tests
+{
+    public class RabbitMqFixture : IAsyncLifetime, IDisposable
+    {
+        private const string DockerNetworkName = "bridgeWhaleNet";
+        private const string RabbitImageName = "rabbitmq";
+        private const string RabbitImageTag = "management";
+        private const string RabbitContainerName = "rmq";
+        private const int DefaultTimeoutSeconds = 20;
+
+        private readonly DockerProxy dockerProxy;
+
+        public RabbitMqFixture()
+        {
+            dockerProxy = new DockerProxy(new Uri(Configuration.DockerHttpApiUri));
+        }
+
+        public async Task InitializeAsync()
+        {
+            await DisposeAsync();
+
+            await dockerProxy.CreateNetworkAsync(DockerNetworkName);
+
+            await dockerProxy.PullImageAsync(RabbitImageName, RabbitImageTag);
+            var portMappings = new Dictionary<string, ISet<string>>
+            {
+                { "4369", new HashSet<string>(){ "4369" } },
+                { "5671", new HashSet<string>(){ "5671" } },
+                { "5672", new HashSet<string>(){ "5672" } } ,
+                { "15671",new HashSet<string>(){ "15671" } },
+                { "15672",new HashSet<string>(){ "15672" } },
+                { "25672",new HashSet<string>(){ "25672" } }
+            };
+            var envVars = new List<string> { "RABBITMQ_DEFAULT_VHOST=test" };
+            var containerId = await dockerProxy.CreateContainerAsync($"{RabbitImageName}:{RabbitImageTag}", RabbitContainerName, portMappings, DockerNetworkName, envVars);
+            await dockerProxy.StartContainerAsync(containerId);
+            await WaitForRabbitMqReady(new CancellationTokenSource(TimeSpan.FromSeconds(DefaultTimeoutSeconds)).Token);
+        }
+
+        public async Task DisposeAsync()
+        {
+            await dockerProxy.StopContainerAsync(RabbitContainerName);
+            await dockerProxy.RemoveContainerAsync(RabbitContainerName);
+            await dockerProxy.DeleteNetworksAsync(DockerNetworkName);
+        }
+
+        public void Dispose()
+        {
+            dockerProxy.Dispose();
+        }
+
+        private static async Task WaitForRabbitMqReady(CancellationToken token)
+        {
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+                if (await IsRabbitMqReady())
+                    return;
+                await Task.Delay(500, token);
+            }
+        }
+
+        private static async Task<bool> IsRabbitMqReady()
+        {
+            var rabbitMqUrl = $"http://{Configuration.RabbitMqHost}:{Configuration.RabbitMqManagementPort}";
+            var rabbitMqManagementApi = new RabbitMqApi(rabbitMqUrl, Configuration.RabbitMqUser, Configuration.RabbitMqPassword);
+
+            try
+            {
+                return await rabbitMqManagementApi.GetDefinitionByVhost(Configuration.RabbitMqVirtualHost) != null;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
