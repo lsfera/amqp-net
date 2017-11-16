69f075daf089a033af6148ae5a06c3b9f501b42a
giordanol <luca.giordano@ynap.com>
2017-11-16 11:21:15 +0100

    Added coverage for queue/exchange creation/deletion

--- src/Amqp.Net.Tests/Integration.cs
+++ src/Amqp.Net.Tests/Integration.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Amqp.Net.Client;
@@ -24,7 +25,7 @@ namespace Amqp.Net.Tests
         [Fact]
         public async Task CreateExchange()
         {
-            const string exchangeName = "test-xchg";
+            const string exchangeName = "test-xchg-create";
             const ExchangeType exchangeType = ExchangeType.Direct;
             const bool isDurable = true;
             const bool isAutodelete = false;
@@ -54,9 +55,35 @@ namespace Amqp.Net.Tests
         }
 
         [Fact]
+        public async Task DeleteExchange()
+        {
+            const string exchangeName = "test-xchg-delete";
+            const ExchangeType exchangeType = ExchangeType.Direct;
+            const bool isUnused = true;
+
+            var exchange = rabbitMqManagementApi.CreateExchange(new ExchangeInfo(exchangeName, exchangeType.ToString().ToLower()), Configuration.RabbitMqVirtualHost);
+            Assert.NotNull(exchange);
+
+            IConnection connection = null;
+            IChannel channel = null;
+            try
+            {
+                (connection, channel) = await BootstrapRabbit();
+                await channel.ExchangeDeleteAsync(exchangeName, isUnused);
+            }
+            finally
+            {
+                channel?.Dispose();
+                connection?.Dispose();
+            }
+
+            Assert.Throws<UnexpectedHttpStatusCodeException>(() => rabbitMqManagementApi.GetExchange(exchangeName, Configuration.RabbitMqVirtualHost));
+        }
+
+        [Fact]
         public async Task CreateQueue()
         {
-            const string queueName = "test-queue";
+            const string queueName = "test-queue-create";
             const bool isDurable = true;
             const bool isExclusive = false;
             const bool isAutoDelete = false;
@@ -82,6 +109,69 @@ namespace Amqp.Net.Tests
             Assert.Equal(retrievedQueue.Exclusive, isExclusive);
             Assert.Equal(retrievedQueue.AutoDelete, isAutoDelete);
         }
+
+        [Fact]
+        public async Task DeleteQueue()
+        {
+            const string queueName = "test-queue-delete";
+            const bool ifUnused = true;
+            const bool ifEmpty = false;
+
+            IConnection connection = null;
+            IChannel channel = null;
+            try
+            {
+                (connection, channel) = await BootstrapRabbit();
+                await channel.QueueDeleteAsync(queueName, ifUnused, ifEmpty);
+            }
+            finally
+            {
+                channel?.Dispose();
+                connection?.Dispose();
+            }
+
+            Assert.Throws<UnexpectedHttpStatusCodeException>(() => rabbitMqManagementApi.GetQueue(queueName, Configuration.RabbitMqVirtualHost));
+
+        }
+
+        [Fact]
+        public async Task QueueBind()
+        {
+            const bool isDurable = true;
+            const string queueName = "test-queue-bind";
+            const bool isExclusive = false;
+            const bool isAutoDelete = false;
+
+            const string exchangeName = "test-xchg-bind";
+            const ExchangeType exchangeType = ExchangeType.Fanout;
+            const bool isAutodelete = false;
+            const bool isInternal = false;
+            const string routingKey = "akey";
+
+            IConnection connection = null;
+            IChannel channel = null;
+            try
+            {
+                (connection, channel) = await BootstrapRabbit();
+                await channel.ExchangeDeclareAsync(exchangeName, exchangeType, isDurable, isAutodelete, isInternal);
+                await channel.QueueDeclareAsync(queueName, isDurable, isExclusive, isAutoDelete);
+                await channel.QueueBindAsync(queueName, exchangeName, routingKey);
+            }
+            finally
+            {
+                channel?.Dispose();
+                connection?.Dispose();
+            }
+
+            var retrievedQueue = rabbitMqManagementApi.GetQueue(queueName, Configuration.RabbitMqVirtualHost);
+            var bindings = rabbitMqManagementApi.GetBindingsForQueue(retrievedQueue).ToList();
+
+            bool BindingFilter(Binding binding) => binding.Source == exchangeName 
+                                            && binding.Destination == queueName 
+                                            && binding.RoutingKey == routingKey;
+            Assert.Contains(bindings, BindingFilter);
+        }
+
 
         private Task<(IConnection connection, IChannel channel)> BootstrapRabbit()
         {
